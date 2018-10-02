1e6ebbd567399472531599dc43c546b564401774
aespinet <aelluvetaa@gmail.com>
2018-10-02 11:45:24 -0400

    Mail Commit 2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const nodemailer = require('nodemailer');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const port = process.env.PORT;
